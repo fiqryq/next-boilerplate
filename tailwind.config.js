7d4acb4f6058322e5558557696fd8140678d632b
fiqryq <fiqrychoerudin48@gmail.com>
2022-07-16 17:01:04 +0700

    chore: update path purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 // @ts-nocheck
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
