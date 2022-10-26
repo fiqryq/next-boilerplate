0219a742242057940fa8b5bef08e9a3e0393f04d
fiqryq <fiqrychoerudin48@gmail.com>
2022-10-26 16:43:02 +0700

    chore: add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+module.exports = createJestConfig(customJestConfig)
