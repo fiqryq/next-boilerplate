ee615b4f8ca96ad42ac599f17e3b4fa1c7c50790
fiqryq <fiqrychoerudin48@gmail.com>
2022-11-20 16:05:05 +0700

    chore: add .pages content config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       aspectRatio: {
