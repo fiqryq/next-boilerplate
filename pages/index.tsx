05c5811295a3010005d7874e2428d1dc2e6720be
fiqryq <fiqrychoerudin48@gmail.com>
2022-12-17 17:17:51 +0700

    fix: delete template #67

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
-import SamplePage from '@/src/components/template/samplePage';
+import ThemeToggle from '@/src/components/themeToggle';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <SamplePage />;
+  return (
+    <div className="bg-[#fafafa]">
+      <div className="relative mx-auto h-screen w-full max-w-7xl bg-white dark:bg-gray-900">
+        <ul className="absolute top-0 flex w-full flex-row-reverse items-center bg-gray-100/50 p-5 dark:bg-gray-800">
+          <li>
+            <ThemeToggle />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
