e506cb70e6ee28862e02a833d8e66b08baebbaea
fiqryq <fiqrychoerudin48@gmail.com>
2022-12-17 17:22:16 +0700

    fix: delete theme toggle #67

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
-import ThemeToggle from '@/src/components/themeToggle';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div className="bg-[#fafafa]">
       <div className="relative mx-auto h-screen w-full max-w-7xl bg-white dark:bg-gray-900">
-        <ul className="absolute top-0 flex w-full flex-row-reverse items-center bg-gray-100/50 p-5 dark:bg-gray-800">
-          <li>
-            <ThemeToggle />
-          </li>
-        </ul>
+        <ul className="absolute top-0 flex w-full flex-row-reverse items-center bg-gray-100/50 p-5 dark:bg-gray-800"></ul>
       </div>
     </div>
   );
